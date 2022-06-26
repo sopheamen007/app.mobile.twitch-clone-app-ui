978590379dc923e23dab43f39c29f0d489451e2c
Sopheamen Van <sopheamen.van@admins-MacBook-Pro-2.local>
2022-06-26 20:06:40 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// @dart=2.9
+
+import 'package:flutter/material.dart';
+import 'package:twitch_app/pages/root_app.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
+
